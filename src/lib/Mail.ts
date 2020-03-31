552637e729f9e1c95b89b6f58660418701927031
Rodolfo Santos Silveira <rodolfo.silveira@soterotech.com.br>
2020-03-31 00:56:35 -0300

    criando queue e mail

---
+++ src/lib/Mail.ts
@@ -0,0 +1,58 @@
+import * as nodemailer from 'nodemailer';
+import * as exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import { resolve } from 'path';
+import mailConfig from '../config/mail';
+
+type Options = {
+  host: string;
+  port: number;
+  secure: boolean;
+  auth: {
+    user: string;
+    pass: string;
+  }
+}
+
+class Mail {
+  private transporter: nodemailer.Transporter
+
+  constructor() {
+    const { host, port, secure, auth } = mailConfig;
+    this.transporter = nodemailer.createTransport<Options>({
+      host,
+      port,
+      secure,
+      auth: auth.user ? auth : null,
+    });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      }),
+    );
+  }
+
+  sendMail(message) {
+    return this.transporter.sendMail({
+      ...mailConfig.default,
+      ...message,
+    });
+  }
+}
+
+export default new Mail();
