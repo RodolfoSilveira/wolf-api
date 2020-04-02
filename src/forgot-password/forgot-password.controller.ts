a0bfb9df29acecef966c1af1ae7594c78dd09527
Rodolfo Santos Silveira <rodolfo.silveira@soterotech.com.br>
2020-04-02 00:46:25 -0300

    configurando esqueceu senha

--- src/forgot-password/forgot-password.controller.ts
+++ src/forgot-password/forgot-password.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Put, Param } from '@nestjs/common';
+import { ForgotPasswordService } from './forgot-password.service';
+import { IUser } from '../user/interface/user.interface';
 
 @Controller('forgot-password')
-export class ForgotPasswordController {}
+export class ForgotPasswordController {
+  constructor(private readonly forgotPasswordService: ForgotPasswordService ){}
+
+  @Post()
+  store(@Body() user: IUser) {
+    return this.forgotPasswordService.store(user.email);
+  }
+
+  @Put(':token')
+  update(@Param() params: any, @Body() user: IUser) {
+    return this.forgotPasswordService.update(params.token, user.password);
+  }
+}
--- src/forgot-password/forgot-password.service.ts
+++ src/forgot-password/forgot-password.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user/entity/user.entity';
 import { Repository } from 'typeorm';
@@ -30,6 +30,28 @@ export class ForgotPasswordService {
         token: user.token
       });
 
+    } catch (err) {
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async update (token: string, password: string): Promise<any> {
+    try {
+      const user = await this.userRepository.findOne({ where: [{token}]});
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        throw new UnauthorizedException();
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await this.userRepository.update({id: user.id}, {...user})
     } catch (err) {
       throw new InternalServerErrorException(err.message);
     }
